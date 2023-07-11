a08d380ea7a839b2d7a94879b499c74056e366a9
MayorTom11 <73259372+MayorTom11@users.noreply.github.com>
2023-07-11 15:50:40 -0300

    CartManager

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,5 +1,27 @@
 import { Router } from "express";
+import { CartManager } from "../CartManager.js";
 
 const router = Router()
+const cartService = new CartManager("../Primera Entrega BE/src/carritos.json")
+
+router.get("/",async(req,res)=>{
+    try {
+        const limit = req.query.limit
+        const getCarts = await cartService.getCarts()
+        const slice1 = getCarts.slice(0,limit)
+        limit == undefined ? res.json(getCarts) : res.json(slice1)
+    } catch (error) {
+        res.json(error.message)
+    }
+})
+router.post("/",async(req,res)=>{
+    try {
+        const newCart = req.body
+        await cartService.addCart(newCart)
+        res.json({message:"Carrito Agregado"})
+    } catch (error) {
+        res.json(error.message)
+    }
+})
 
 export {router as cartsRouter}
